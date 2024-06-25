fc51c693bea337b1f4fa92dd059292af7be19518
Leandro Lorenzini <leandro.lorenzini@mlytics.com>
2024-06-25 10:20:36 +0800

    Removed rate limit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const helmet = require("helmet");
 
 const route = require("./src/route");
 const jumpcloud = require("./src/jumpcloud");
@@ -8,12 +7,6 @@ const jumpcloud = require("./src/jumpcloud");
 const app = express();
 const port = 3000;
 
-// Protect application
-const limiter = rateLimit({
-	windowMs: 10 * 60 * 1000, // 10 minutes
-	limit: 15, // Limit each IP to 10 requests per minute.
-})
-app.use(limiter)
 app.use(helmet());
 
 // Needed for slack signing verification
@@ -34,17 +27,23 @@ app.listen(port, () => {
 
 // Remove admin rights for admin requests older than 30 minutes
 setInterval(() => {
-  const thirtyMinutesAgo = Date.now() - (30 * 60 * 1000);
-  jumpcloud.getAllSudoUsers().then(users => {
-    for (let user of users) {
-      for (let attr of user.attributes) {
-        if (attr.name === "LastAdminRequest" && attr.value < thirtyMinutesAgo) {
-          console.log(`Revoking sudo access from ${user.username}`);
-          jumpcloud.updateUser(user._id, false);
+  const thirtyMinutesAgo = Date.now() - 30 * 60 * 1000;
+  jumpcloud
+    .getAllSudoUsers()
+    .then((users) => {
+      for (let user of users) {
+        for (let attr of user.attributes) {
+          if (
+            attr.name === "LastAdminRequest" &&
+            attr.value < thirtyMinutesAgo
+          ) {
+            console.log(`Revoking sudo access from ${user.username}`);
+            jumpcloud.updateUser(user._id, false);
+          }
         }
       }
-    }
-  }).catch(error => {
-    console.log(error);
-  });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }, 10000);
